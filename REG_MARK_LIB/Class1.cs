47e2960179a3567aed1c27b963132a27bf4dafe7
CoolVeryVery <gorgn2005@gmail.com>
2025-03-06 15:02:31 +0300

    cont

--- REG_MARK_LIB/Class1.cs
+++ REG_MARK_LIB/Class1.cs
@@ -1,12 +1,12 @@
 ﻿namespace REG_MARK_LIB
 {
-    public class REG_MARK_LIB
+    public class REG_MARK_LIB_CLASS
     {
-        List<char> correctSymbols = new List<char>() { 'A', 'B', 'E', 'K', 'M', 'H', 'O', 'P', 'C', 'T', 'Y', 'X'};
-        List<int> correctNumRegion = new List<int>() { 102, 113, 116, 716, 121, 123, 124, 125, 134, 150, 190, 750, 790, 152, 154, 159, 161, 163, 164, 196, 173, 174, 177, 197, 199, 777, 799, 186 };
+        List<char> _correctSymbols = new List<char>() { 'a', 'b', 'e', 'k', 'm', 'h', 'o', 'p', 'c', 't', 'y', 'x'};
+        List<int> _correctNumRegion = new List<int>() { 102, 113, 116, 716, 121, 123, 124, 125, 134, 150, 190, 750, 790, 152, 154, 159, 161, 163, 164, 196, 173, 174, 177, 197, 199, 777, 799, 186 };
         public bool CheckMark(string mark)
         {
-            switch (correctSymbols.Contains(mark[0]) && correctSymbols.Contains(mark[4]) && correctSymbols.Contains(mark[5]))
+            switch (_correctSymbols.Contains(mark[0]) && _correctSymbols.Contains(mark[4]) && _correctSymbols.Contains(mark[5]))
             {
                 case false:
                     return false;
@@ -15,108 +15,170 @@
             switch (mark.Length)
             {
                 case 9:
-                    numRegion = Convert.ToInt32(mark[7] + mark[8] + mark[9]);
+                    numRegion = Convert.ToInt32(mark[6..9]);
                     break;
                 case 8:
-                    numRegion = Convert.ToInt32(mark[8] + mark[9]);
+                    numRegion = Convert.ToInt32(mark[6..8]);
                     break;
             }
             if (0 < numRegion && numRegion < 100)
             {
                 return true;
             }
-            if (correctNumRegion.Contains(numRegion))
+            if (_correctNumRegion.Contains(numRegion))
             {
                 return true;
             }
             return false;
         }
-        string GetNextMarkAfter(string mark)
+        public string GetNextMarkAfter(string mark)
         {
             string resultNum = string.Copy(mark);
-            int numberMark = Convert.ToInt32(mark[1..3]);
+            int numberMark = Convert.ToInt32(mark[1..4]);
             if (numberMark != 999)
             {
-                string numberMarkStr = numberMark++.ToString();
-                resultNum.Replace(resultNum[1], numberMarkStr[0]);
-                resultNum.Replace(resultNum[2], numberMarkStr[1]);
-                resultNum.Replace(resultNum[3], numberMarkStr[2]);
+                string numberMarkStr = (++numberMark).ToString();
+                switch (numberMarkStr.Length)
+                {
+                    case 1:
+                        numberMarkStr = String.Concat("00", numberMarkStr);
+                        break;
+                    case 2:
+                        numberMarkStr = String.Concat("0", numberMarkStr);
+                        break;
+                }
+                resultNum = resultNum.Remove(1, 1).Insert(1, numberMarkStr[0].ToString());
+                resultNum = resultNum.Remove(2, 1).Insert(2, numberMarkStr[1].ToString()); 
+                resultNum = resultNum.Remove(3, 1).Insert(3, numberMarkStr[2].ToString()); 
                 return resultNum;
             }
             else
             {
-                resultNum.Replace(resultNum[1], '0');
-                resultNum.Replace(resultNum[2], '0');
-                resultNum.Replace(resultNum[3], '0');
-                int countEndSymbol = resultNum.Count(x => x == 'X');
+                resultNum = resultNum.Remove(1, 1).Insert(1, "0");
+                resultNum = resultNum.Remove(2, 1).Insert(2, "0");
+                resultNum = resultNum.Remove(3, 1).Insert(3, "0");
+                int countEndSymbol = resultNum.Count(x => x == 'x');
                 if (countEndSymbol == 3)
                 {
-                    resultNum.Replace(resultNum[0], 'A');
-                    resultNum.Replace(resultNum[4], 'A');
-                    resultNum.Replace(resultNum[5], 'A');
+                    resultNum = resultNum.Remove(0, 1).Insert(0, "a"); 
+                    resultNum = resultNum.Remove(4, 1).Insert(4, "a");
+                    resultNum = resultNum.Remove(5, 1).Insert(5, "a");
                     return resultNum;
                 }
                 else if (countEndSymbol == 2)
                 {
-                    int indexFirstLastSymbol = resultNum.IndexOf('X');
-                    int indexSecondLastSymbol = resultNum.LastIndexOf('X');
+                    int indexFirstLastSymbol = resultNum.IndexOf('x');
+                    int indexSecondLastSymbol = resultNum.LastIndexOf('x');
                     if (indexFirstLastSymbol == 0 && indexSecondLastSymbol == 4)
                     {
-                        int indexInList = correctSymbols.IndexOf(resultNum[5]);
-                        resultNum.Replace(resultNum[5], correctSymbols[indexInList++]);
+                        int indexInList = _correctSymbols.IndexOf(resultNum[5]);
+                        resultNum = resultNum.Remove(5, 1).Insert(5, _correctSymbols[indexInList++].ToString());
                         return resultNum;
                     }
                     else if (indexFirstLastSymbol == 0 && indexSecondLastSymbol == 5)
                     {
-                        int indexInList = correctSymbols.IndexOf(resultNum[4]);
-                        resultNum.Replace(resultNum[4], correctSymbols[indexInList++]);
+                        int indexInList = _correctSymbols.IndexOf(resultNum[4]);
+                        resultNum = resultNum.Remove(4, 1).Insert(4, _correctSymbols[indexInList++].ToString());
                         return resultNum;
                     }
                     else
                     {
-                        int indexInList = correctSymbols.IndexOf(resultNum[0]);
-                        resultNum.Replace(resultNum[0], correctSymbols[indexInList++]);
+                        int indexInList = _correctSymbols.IndexOf(resultNum[0]);
+                        resultNum = resultNum.Remove(0, 1).Insert(0, _correctSymbols[indexInList++].ToString());
                         return resultNum;
                     }
                 }
                 else
                 {
-                    switch(resultNum.IndexOf('X'))
+                    switch(resultNum.IndexOf('x'))
                     {
                         case 0:
-                            int indexInList = correctSymbols.IndexOf(resultNum[5]);
-                            resultNum.Replace(resultNum[5], correctSymbols[indexInList++]);
+                            int indexInList = _correctSymbols.IndexOf(resultNum[5]);
+                            resultNum = resultNum.Remove(5, 1).Insert(5, _correctSymbols[indexInList++].ToString());
                             return resultNum;
                         case 4:
-                            indexInList = correctSymbols.IndexOf(resultNum[5]);
-                            resultNum.Replace(resultNum[5], correctSymbols[indexInList++]);
+                            indexInList = _correctSymbols.IndexOf(resultNum[5]);
+                            resultNum = resultNum.Remove(5, 1).Insert(5, _correctSymbols[indexInList++].ToString());
                             return resultNum;
                         case 5:
-                            indexInList = correctSymbols.IndexOf(resultNum[4]);
-                            resultNum.Replace(resultNum[4], correctSymbols[indexInList++]);
-                            resultNum.Replace(resultNum[5], 'A');
+                            indexInList = _correctSymbols.IndexOf(resultNum[4]);
+                            resultNum = resultNum.Remove(4, 1).Insert(4, _correctSymbols[indexInList++].ToString());
+                            resultNum = resultNum.Remove(5, 1).Insert(5, "a");
                             return resultNum;
                     }
                 }
             }
             return resultNum;
         }
-        //string GetNextMarkAfterInRange(string prevMark, string rangeStart, string rangeEnd)
-        //{
-        //    int numberMark = Convert.ToInt32(prevMark[1..3]);
-        //    if (numberMark < Convert.ToInt32(rangeStart[1..3]) || numberMark > Convert.ToInt32(rangeEnd[1..3]))
-        //    {
-        //        return "out of stock";
-        //    }
-        //    else if (numberMark == 999)
-        //    {
+        public string GetNextMarkAfterInRange(string prevMark, string rangeStart, string rangeEnd)
+        {
+            int indexFirstSymbolRangeStart = _correctSymbols.IndexOf(rangeStart[0]);
+            int indexFirstSymbolRangeEnd = _correctSymbols.IndexOf(rangeEnd[0]);
+            int indexFirstSymbolPrevMark = _correctSymbols.IndexOf(prevMark[0]);
+            if (indexFirstSymbolRangeStart <= indexFirstSymbolPrevMark && indexFirstSymbolPrevMark <= indexFirstSymbolRangeEnd)
+            {
+                return GetNextMarkAfter(prevMark);
+            }
+            else if (indexFirstSymbolRangeStart == indexFirstSymbolRangeEnd && indexFirstSymbolRangeStart == indexFirstSymbolPrevMark)
+            {
+                int indexSecondSymbolRangeStart = _correctSymbols.IndexOf(rangeStart[4]);
+                int indexSecondSymbolRangeEnd = _correctSymbols.IndexOf(rangeEnd[4]);
+                int indexSecondSymbolPrevMark = _correctSymbols.IndexOf(prevMark[4]);
+                if (indexSecondSymbolRangeStart <= indexSecondSymbolPrevMark && indexSecondSymbolPrevMark <= indexSecondSymbolRangeEnd)
+                {
+                    return GetNextMarkAfter(prevMark);
+                }
+                else if (indexSecondSymbolRangeStart == indexSecondSymbolRangeEnd && indexSecondSymbolRangeStart == indexSecondSymbolPrevMark)
+                {
+                    int indexThirdSymbolRangeStart = _correctSymbols.IndexOf(rangeStart[5]);
+                    int indexThirdSymbolRangeEnd = _correctSymbols.IndexOf(rangeEnd[5]);
+                    int indexThirdSymbolPrevMark = _correctSymbols.IndexOf(prevMark[5]);
+                    if (indexThirdSymbolRangeStart <= indexThirdSymbolPrevMark && indexThirdSymbolPrevMark <= indexThirdSymbolRangeEnd)
+                    {
+                        return GetNextMarkAfter(prevMark);
+                    }
+                    else if (indexThirdSymbolRangeStart == indexThirdSymbolRangeEnd && indexThirdSymbolRangeStart == indexThirdSymbolPrevMark)
+                    {
+                        int numberRangeStart = Convert.ToInt32(rangeStart[1..3]);
+                        int numberRangeEnd = Convert.ToInt32(rangeEnd[1..3]);
+                        int numberPevMark = Convert.ToInt32(prevMark[1..3]);
+                        if (numberRangeStart < numberPevMark && numberPevMark < numberRangeEnd)
+                        {
+                            return GetNextMarkAfter(prevMark);
+                        }
+                        else return "out of stock";
+                    }
+                    else
+                    {
+                        return "out of stock";
+                    }
+                }
+                else
+                {
+                    return "out of stock";
+                }
+            }
+            else
+            {
+                return "out of stock";
+            }
+        }
+        int GetCombinationsCountInRange(string mark1, string mark2)
+        {
+            int indexFirstSymbolMarkOne = _correctSymbols.IndexOf(mark1[0]);
+            int indexFirstSymbolMarkTwo = _correctSymbols.IndexOf(mark2[0]);
+            switch (indexFirstSymbolMarkOne == indexFirstSymbolMarkTwo)
+            {
+                case true:
+                    int numberMarkOne = Convert.ToInt32(mark1[1..4]);
+                    int numberMarkTwo = Convert.ToInt32(mark1[1..4]);
 
-        //    }
-        //}
-        //int GetCombinationsCountInRange(string mark1, string mark2)
-        //{
+                    break;
+                case false:
+                    break;
+            }
 
-        //}
+        }
 
     }
 }
---
+++ TestREG_MARK_LIB/Test1.cs
@@ -0,0 +1,62 @@
+﻿using REG_MARK_LIB;
+
+namespace TestREG_MARK_LIB
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void Test_FirstAutoNumber_CheckMark_IsTrue()
+        {
+            REG_MARK_LIB_CLASS markLibClass = new REG_MARK_LIB_CLASS();
+            bool result = markLibClass.CheckMark("a321be32");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Test_SecondAutoNumber_CheckMark_IsTrue()
+        {
+            REG_MARK_LIB_CLASS markLibClass = new REG_MARK_LIB_CLASS();
+            bool result = markLibClass.CheckMark("a000aa159");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Test_ThirtAutoNumber_CheckMark_IsTrue()
+        {
+            REG_MARK_LIB_CLASS markLibClass = new REG_MARK_LIB_CLASS();
+            bool result = markLibClass.CheckMark("x999xx196");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void Test_FirstAutoNumber_GetNextMarkAfter_AreEqual()
+        {
+            REG_MARK_LIB_CLASS markLibClass = new REG_MARK_LIB_CLASS();
+            string result = markLibClass.GetNextMarkAfter("x999xx196");
+            string exprcted = "a000aa196";
+            Assert.AreEqual(exprcted, result);
+        }
+        [TestMethod]
+        public void Test_SecondAutoNumber_GetNextMarkAfter_AreEqual()
+        {
+            REG_MARK_LIB_CLASS markLibClass = new REG_MARK_LIB_CLASS();
+            string result = markLibClass.GetNextMarkAfter("a000mh19");
+            string exprcted = "a001mh19";
+            Assert.AreEqual(exprcted, result);
+        }
+        [TestMethod]
+        public void Test_FitstAutoNumber_GetNextMarkAfterInRange_AreEqual()
+        {
+            REG_MARK_LIB_CLASS markLibClass = new REG_MARK_LIB_CLASS();
+            string result = markLibClass.GetNextMarkAfterInRange("a050aa19", "a000aa19", "a100aa19");
+            string exprcted = "a051aa19";
+            Assert.AreEqual(exprcted, result);
+        }
+        [TestMethod]
+        public void Test_SecondAutoNumber_GetNextMarkAfterInRange_AreEqual()
+        {
+            REG_MARK_LIB_CLASS markLibClass = new REG_MARK_LIB_CLASS();
+            string result = markLibClass.GetNextMarkAfterInRange("m050ty19", "e999mk19", "x999aa19");
+            string exprcted = "m051ty19";
+            Assert.AreEqual(exprcted, result);
+        }
+    }
+}
